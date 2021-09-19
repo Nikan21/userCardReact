87d086e09baffd2865de1529927ae9e60d0d97b1
Dmitriy Nikanorov <dimonnikanorov2@gmail.com>
2021-09-19 23:21:04 +0300

    Add <img>

---
+++ src/Components/userAvatar/index.js
@@ -0,0 +1,10 @@
+import Avatar from './avatar.png'
+import './avatar.css'
+
+function avatar (){
+    return (
+        <img src={Avatar} className='avatar' alt='avatar'/>
+    );
+}
+
+export default avatar;
