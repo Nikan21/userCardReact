3b9cab6ae3c5f257c9501a40a160ca56d2a3e0c9
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-09-20 20:30:39 +0300

    Add new files

---
+++ src/Components/userCard/indes.jsx
@@ -0,0 +1,10 @@
+import Avatar from './../userAvatar/index.jsx'
+
+function card (){
+    return(
+        <div className='headerCard'>
+            <Avatar />
+            <p></p>
+        </div>
+    )
+}
