bea30bd7803db7e5f8f93c85bdfb9dd20beffe5f
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-09-21 19:44:20 +0300

    Add object

--- src/Components/userCard/index.jsx
+++ src/Components/userCard/index.jsx
@@ -1,9 +1,13 @@
 import Avatar from '../userAvatar/index.jsx'
 
+const avatar = {
+    photo: '/Damian.png'
+}
+
 function card (){
     return(
         <div className='headerCard'>
-            <Avatar />
+            <Avatar photo = {avatar.photo}/>
             <p></p>
         </div>
     )
