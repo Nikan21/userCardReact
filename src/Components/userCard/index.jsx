d01d6b5f69f3bb18288ae5277b39da92a8d18171
Dima Nikanorov <dimonnikanorov2@gmail.com>
2021-09-22 19:38:03 +0300

    Finish card with props

--- src/Components/userCard/index.jsx
+++ src/Components/userCard/index.jsx
@@ -1,17 +1,24 @@
-import Avatar from '../userAvatar/index.jsx'
-import './card.css'
+import Avatar from "../userAvatar/index.jsx";
+import "./card.css";
 
-const avatar = '/Damian.png';
+const avatar = "/Damian.png";
 
-function card (props){
-    const {name, surname, description} = props.data;
+function card(props) {
+  const { name, surname, description } = props.data;
 
-    return(
-        <div className='headerCard'>
-            <Avatar photo = {avatar}/>
-            <div className='userName'>{name} {surname}</div>
+  return (
+    <div className='Card'>
+      <div className="headerCard">
+        <Avatar photo={avatar} />
+        <div className="userName">
+          {name} {surname}
         </div>
-    )
+      </div>
+      <div className='mainCard'>
+        <p className='description'>{description}</p>
+      </div>
+    </div>
+  );
 }
 
-export default card
+export default card;
